871c270cb94e657a9c73b6721c73ba5f9cf34c42
dirk <dirk.giese70@gmail.com>
2024-09-04 05:31:52 +0000

    commit sandbox

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,34 +1,32 @@
-import {projects, webs}  from "../../constants/constants.js";
-import './project.css'
+import { projects } from "../../constants/constants.js";
+import "./project.css";
 const Project = () => {
-
-    return (
-        <div className="dgi__project-grid">
-            {projects.map((p,i) => (
-                <div key={p.id}>
-                    <h3>{p.title}</h3>
-                    <img src={p.image}/>
-                    <div className="card-info">
-                        {p.description}
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="{p.visit}">Code</a>
-                        </li>
-                        <li>
-                            <a href="{p.source}">soure</a>
-                        </li>
-                    </ul>
-                </div>
-            ))}
-            <div className="dgi_project-grid">
-                {webs.map((w,i)=>{
-                    <div key={w.stars}>
-                        <h3>{w.name}</h3>
-                    </div>
-                })}
+  console.log(projects);
+  return (
+    <div className="dgi__project-grid">
+      {projects.map((p, i) => (
+        <div className="dgi__project-card">
+          <div className="dgi__project-image">
+            <a href={p.url} target="_blank" titel={p.name}>
+                <span className="dgi__project-year">{p.year}</span>
+                <img src={p.img}  alt={p.name}/>
+            </a>
+          </div>
+          <div className="dgi__project-info">
+          <h3>{p.name}</h3>
+            {p.description}
             </div>
+          <ul>
+            <li>
+              <a href="{p.visit}">Code</a>
+            </li>
+            <li>
+              <a href="{p.source}">soure</a>
+            </li>
+          </ul>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 export default Project;
